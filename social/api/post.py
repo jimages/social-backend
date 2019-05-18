dc20fce1d3c69aedbb90d215e501bc794977ad2a
Zachary Wang <jimages123@gmail.com>
2019-05-18 19:08:26 +0800

    添加了发布

--- social/api/post.py
+++ social/api/post.py
@@ -7,7 +7,7 @@ from flask import request, current_app, g, abort
 from flask_restful import Resource
 from social import model, schemas, exception, utils
 from social.utils import authRequired
-
+from sqlalchemy import or_, and_
 __all__ = [
     'Post',
     'Upvote',
@@ -15,6 +15,7 @@ __all__ = [
 ]
 
 post_schema = schemas.PostSchema()
+posts_schema = schemas.PostSchema(many=True)
 comment_schema = schemas.CommentSchema()
 
 class Post(Resource):
@@ -49,6 +50,35 @@ class Post(Resource):
             raise exception.DataInvaliError("数据错误")
 
         return utils.ok(data.data, 201)
+
+    @authRequired
+    def get(self):
+        after = request.args.get('after' ,None ,int)
+        type = request.args.get('type', 'all', str)
+
+        if type == 'all' and after is None:
+            # 这里的所有表示的是又包括自己的学校还有自己的学院
+            posts = model.Post.query.filter(or_(and_(model.Post.publish_type == 'institute', model.Post.publish_id == g.current_user.institute_id),
+                model.Post.publish_type == 'all')).order_by(model.Post.id.desc()).limit(30).all()
+
+        elif type == 'all' and after is not None:
+            posts = model.Post.query.filter(or_(and_(model.Post.publish_type == 'institute', model.Post.publish_id == g.current_user.institute_id),
+                model.Post.publish_type == 'all')).filter(model.Post.id < after).order_by(model.Post.id.desc()).limit(30).all()
+        elif type == "institute" and after is None:
+            posts = model.Post.query.filter(and_(model.Post.publish_type == 'institute', model.Post.publish_id == g.current_user.institute_id)).order_by(model.Post.id.desc()).limit(30).all()
+
+        elif type == "institute" and after is not None:
+            posts = model.Post.query.filter(and_(model.Post.publish_type == 'self', model.Post.publish_id == g.current_user.institute_id)).filter(model.Post.id < after).order_by(model.Post.id.desc()).limit(30).all()
+
+        elif after is not None:
+            posts = model.Post.query.filter(and_(model.Post.publish_type == 'self', model.Post.publish_id == g.current_user.id)).order_by(model.Post.id.desc()).limit(30).all()
+        else:
+            posts = model.Post.query.filter(and_(model.Post.publish_type == 'self', model.Post.publish_id == g.current_user.id, model.Post.id < after)).order_by(model.Post.id.desc()).limit(30).all()
+
+        posts = posts_schema.dump(posts)
+
+        return utils.ok(posts.data)
+
 
 class Upvote(Resource):
     @authRequired
